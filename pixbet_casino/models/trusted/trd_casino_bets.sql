57351e46521a14fea4f5869f32624e5180d5f725
matheus-amon <matheus.amon@outlook.com>
2025-05-12 17:32:34 -0300

    feat(trusted): renomeado colunas na view trd_casino_bets e add view trd_casino_games model

--- pixbet_casino/models/trusted/trd_casino_bets.sql
+++ pixbet_casino/models/trusted/trd_casino_bets.sql
@@ -1,14 +1,14 @@
 with changed_type_and_rename_columns as (
     select
-        cast(id as integer) as id_aposta,
-        cast(win as double precision) as valor_ganho,
-        cast(stake as double precision) as valor_aposta,
-        cast(game_id as integer) as id_jogo,
-        cast(user_id as integer) as id_usuario,
-        cast(created_at as date) as data_atualizacao,
-        cast(to_char(created_at, 'HH24:MI:SS') as time) as hora_criacao,
-        cast(is_freespin as boolean) as rodada_gratis
-    from {{ ref('src_casino_bets') }}
+        cast(id as integer) as bet_id,
+        cast(win as double precision) as win_amount,
+        cast(stake as double precision) as bet_amount,
+        cast(game_id as varchar) as game_id,
+        cast(user_id as integer) as user_id,
+        cast(created_at as date) as created_date,
+        cast(created_at as time) as creation_time,
+        cast(is_freespin as boolean) as is_free_spin
+        from {{ ref('src_casino_bets') }}
 )
 
 select * from changed_type_and_rename_columns
---
+++ pixbet_casino/models/trusted/trd_casino_games.sql
@@ -0,0 +1,5 @@
+with changed_type_and_rename_columns as (
+    select
+        cast(id as integer) as game_id,
+        
+)
