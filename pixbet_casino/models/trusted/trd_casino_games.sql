5d035e45727ef824a7dd72577ce7756de72ef2fd
matheus-amon <matheus.amon@outlook.com>
2025-05-12 23:37:36 -0300

    feat(trusted): casting feito e renomeado campos trd_casino_games model

--- pixbet_casino/models/trusted/trd_casino_games.sql
+++ pixbet_casino/models/trusted/trd_casino_games.sql
@@ -1,5 +1,15 @@
 with changed_type_and_rename_columns as (
     select
-        cast(id as integer) as game_id,
-        
+        cast(id as varchar) as game_id,
+        cast(name as varchar) as game_name,
+        cast(active as boolean) as game_active,
+        cast(category as varchar) as game_category,
+        cast(created_at as date) as created_date,
+        cast(environment as varchar) as game_environment,
+        cast(provider_id as integer) as game_provider,
+        cast(mapped_category as varchar) as sigap_category
+    from {{ ref('src_casino_games') }}
 )
+
+select * from changed_type_and_rename_columns
+where game_active = true
