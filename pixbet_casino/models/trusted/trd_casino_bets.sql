d66512843d418edc61e9f62a9f1319a519adc56f
matheus-amon <matheus.amon@outlook.com>
2025-05-13 18:51:49 -0300

    feat(trusted): reorder and enhance column casting in trd_casino_bets model

--- pixbet_casino/models/trusted/trd_casino_bets.sql
+++ pixbet_casino/models/trusted/trd_casino_bets.sql
@@ -1,12 +1,14 @@
 with changed_type_and_rename_columns as (
     select
         cast(id as integer) as bet_id,
-        cast(win as double precision) as win_amount,
-        cast(stake as double precision) as bet_amount,
         cast(game_id as varchar) as game_id,
-        cast(user_id as integer) as user_id,
         cast(created_at as date) as created_date,
         cast(created_at as time) as creation_time,
+        cast(user_id as integer) as user_id,
+        cast(stake as double precision) as bet_amount,
+        cast(win as double precision) as win_amount,
+        cast(stake - win as double precision) as ggr,
+        cast((stake - win)-((stake - win) * 0.12) as double precision) as ngr,
         cast(is_freespin as boolean) as is_free_spin
         from {{ ref('src_casino_bets') }}
 )
