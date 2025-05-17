29fafc316de9714326a413a56b585e67baf993d2
matheus-amon <matheus.amon@outlook.com>
2025-05-17 16:16:06 -0300

    fix: refactor gaming performance query to improve readability and maintainability

--- casino_example/models/prod/fct_gaming_perfomance.sql
+++ casino_example/models/prod/fct_gaming_perfomance.sql
@@ -1,11 +1,6 @@
-with joined_tables as (
-    select 
-        g.game_id,
-        g.game_name,
-        g.game_environment,
-        g.game_provider,
-        g.sigap_category,
-
+with gaming_performance_base as (
+    select
+        w.game_id,
         w.date,
         w.wins,
         w.gain,
@@ -15,52 +10,49 @@ with joined_tables as (
         w.rtp,
         w.real_gain,
         w.fsb_gain,
-
         r.turnover,
         r.avg_turnover_per_user,
         r.avg_ticket,
         r.ggr,
         r.margin,
         r.fsb_ggr,
-
         p.bets,
         p.bets_per_user,
         p.free_spins
-    from {{ ref('dim_casino_games') }} as g
-    inner join {{ ref('stg_winnings') }} as w
-        on g.game_id = w.game_id and g.created_date = w.date
-    inner join {{ ref('stg_revenue') }} as r
+    from {{ ref('stg_winnings') }} as w
+    left join {{ ref('stg_revenue') }} as r
         on w.game_id = r.game_id and w.date = r.date
-    inner join {{ ref('stg_popularity') }} as p
+    left join {{ ref('stg_popularity') }} as p
         on w.game_id = p.game_id and w.date = p.date
 )
 
 select
-    game_id as nk_game_id,
+    g.game_id as nk_game_id,
 
-    game_name as dsc_game_name,
-    game_environment as dsc_game_environment,
-    game_provider as dsc_game_provider,
-    sigap_category as dsc_sigap_category,
+    g.game_name as dsc_game_name,
+    g.game_environment as dsc_game_environment,
+    g.game_provider as dsc_game_provider,
+    g.sigap_category as dsc_sigap_category,
 
-    date as dt_date,
+    b.date as dt_date,
 
-    wins as mtr_wins,
-    gain as mtr_gain,
-    winners as mtr_winners,
-    avg_gain as mtr_avg_gain,
-    avg_gain_per_user as mtr_avg_gain_per_user,
-    rtp as mtr_rtp,
-    real_gain as mtr_real_gain,
-    fsb_gain as mtr_fsb_gain,
-    turnover as mtr_turnover,
-    avg_turnover_per_user as mtr_avg_turnover_per_user,
-    avg_ticket as mtr_avg_ticket,
-    ggr as mtr_ggr,
-    margin as mtr_margin,
-    fsb_ggr as mtr_fsb_ggr,
-    bets as mtr_bets,
-    bets_per_user as mtr_bets_per_user,
-    free_spins as mtr_fsbs
-
-from joined_tables
+    b.wins as mtr_wins,
+    b.gain as mtr_gain,
+    b.winners as mtr_winners,
+    b.avg_gain as mtr_avg_gain,
+    b.avg_gain_per_user as mtr_avg_gain_per_user,
+    b.rtp as mtr_rtp,
+    b.real_gain as mtr_real_gain,
+    b.fsb_gain as mtr_fsb_gain,
+    b.turnover as mtr_turnover,
+    b.avg_turnover_per_user as mtr_avg_turnover_per_user,
+    b.avg_ticket as mtr_avg_ticket,
+    b.ggr as mtr_ggr,
+    b.margin as mtr_margin,
+    b.fsb_ggr as mtr_fsb_ggr,
+    b.bets as mtr_bets,
+    b.bets_per_user as mtr_bets_per_user,
+    b.free_spins as mtr_fsbs
+from gaming_performance_base as b
+inner join {{ ref('dim_casino_games') }} as g
+    on b.game_id = g.game_id
