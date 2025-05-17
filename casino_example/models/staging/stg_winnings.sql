409ae076460a2581be69e48cf8ce5062aa5a9784
matheus-amon <matheus.amon@outlook.com>
2025-05-17 13:54:30 -0300

    fix(staging): correct CTE name from winnings to winning for consistency

--- casino_example/models/staging/stg_winnings.sql
+++ casino_example/models/staging/stg_winnings.sql
@@ -1,5 +1,6 @@
-with winnings as (
+with winning as (
     select
+        cb.bet_id,
         cb.game_id,
         cb.created_date as date,
         cb.user_id,
@@ -13,4 +14,21 @@ with winnings as (
         {{ ref('trd_casino_games') }} as cg
         on cb.game_id = cg.game_id
 )
+
+select
+    game_id,
+    date,
+    count(case when win_amount > 0 then distinct bet_id end) as wins,
+    sum(win_amount) as win_amount,
+    count(case when win_amount > 0 then distinct user_id end) as winners,
+    count(distinct user_id) / sum(win_amount) as avg_gain,
+    sum(win_amount) / count(distinct user_id) as avg_win,
+    sum(win_amount) / sum(bet_amount) as rtp,
+    sum(case when cb.ggr > 0 then win_amount else 0 end) as real_gain,
+    sum(case when cb.fsb = 1 then win_amount else 0 end) as fsb_gain
+from
+    winning
+group by
+    game_id,
+    date
 
